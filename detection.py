9fbaaa55a74e0ad9866e98db24f5dea561233eab
DinosaurPotato <99208081+DinosaurPotato@users.noreply.github.com>
2022-07-10 18:02:25 -0400

    Update detection.py

--- detection.py
+++ detection.py
@@ -12,7 +12,7 @@ detection = False
 detection_stopped_time = None
 timer_started = False
 SECONDS_TO_RECORD_AFTER_DETECTION = 10
-URL = "https://maker.ifttt.com/trigger/detection/with/key/cGyB8-dfd9ypbFlznJftnX"
+URL = "(URL)"
 
 frame_size = (int(cap.get(3)), int(cap.get(4)))
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
